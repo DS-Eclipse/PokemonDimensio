1a991840abf1dd14f886acb5efc1551074491ada
nervuwus <nightingalenervous@gmail.com>
2023-03-06 16:44:42 +0100

    Simple API part

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import config from "../environments.config";
+
+import { root } from "./router";
+//import { connectDatabase } from "./database/connectDatabase";
+
+const app = express();
+const port: string = config.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(root);
+
+app.listen(port, () => {
+  console.log(`* [${port}] server running at http://localhost:${port}`));
+});
---
+++ api/src/router/root.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from "express";
+
+const root = Router();
+
+root.get("/", (req: Request, res: Response) => {
+  console.log(`* [Redirection] Redirected to /`));
+  res.send({ message: "Express is working" });
+});
+
+export default root;
