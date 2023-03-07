2c84b8b9de3b8b2af4c63addc32f04fdfa733c27
nervuwus <nightingalenervous@gmail.com>
2023-03-07 18:08:21 +0100

    Add View Encapsulation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,10 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = 'Home';
 
   constructor(private router: Router){}
 
