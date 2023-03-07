6d2968bbf3a04d23017209a8f9fb34327bbd43cf
AngeluChan <111064271+AngeluChan@users.noreply.github.com>
2023-03-07 13:35:20 +0100

    Bug fix
    connection.component inexistent, delete lines on module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConnectionPageComponent } from './connection-page/connection-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectionPageComponent,
     LoginPageComponent,
     SignupPageComponent
   ],
